1cf20550e8ad8717f22bea51f7bfe3d2aacc8279
MSaudTahir <68624559+MSaudTahir@users.noreply.github.com>
2023-02-04 15:11:53 +0500

    Create wallet.py

---
+++ wallet.py
@@ -0,0 +1,12 @@
+class Wallet():
+    def __init__(self):
+        self.balance = 0
+        
+    def setAmount(self, amount):
+        self.balance = amount
+    
+    def getAmount(self):
+        return self.balance
+    
+    def removeAmount(self, amount):
+        self.balance = self.balance - amount
