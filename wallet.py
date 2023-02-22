a87e10fbdf5fedc8f44d1ac9e485e4bea16afafe
MSaudTahir <68624559+MSaudTahir@users.noreply.github.com>
2023-02-22 15:24:55 +0500

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -1,18 +1,16 @@
 class Wallet:
-    def __init__(self):
-        self.balance = 0
+  def __init__(self):
+    self.balance = 0
 	
-        
-    def setAmount(self, amount):
-        self.balance = amount
-	
+  def setAmount(self, amount):
+    self.balance = amount
     
-    def getAmount(self):
-        return self.balance
+  def getAmount(self):
+    return self.balance
     
-    def addAmount(self, amount):
-	self.balance = self.balance + amount
+  def addAmount(self, amount):
+	  self.balance = self.balance + amount
 	
-    def removeAmount(self, amount):
-        self.balance = self.balance - amount
+  def removeAmount(self, amount):
+    self.balance = self.balance - amount
 
